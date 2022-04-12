18d7dd604a902f9253220ca5d66fbd4a4d7c05a3
Augusto <augustinhobrito542@gmail.com>
2022-04-12 14:38:59 -0300

    utiliza o is_array() para verificar se a variavel é um array

---
+++ exemplos.php
@@ -0,0 +1,15 @@
+<?php
+
+$valores = [
+    'real' => 8,
+    'bla' => 9,
+    'aa' => 9,
+    'iii' => 7,
+    'xxx' => 6,
+    'zzz' => 5,
+
+];
+
+if (is_array($valores)) {
+    echo 'sim, isto é um array';
+}

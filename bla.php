1de3ba47e858e5bc9a17087244bc2cbe5a55ad7c
Augusto <augustinhobrito542@gmail.com>
2022-04-12 10:06:25 -0300

    definindo criterio

--- bla.php
+++ bla.php
@@ -1,19 +1,32 @@
 <?php
 
 $notas = [
-    10,
-    8,
-    9,
-    7
-
+    [
+        'Aluno' => 'Maria',
+        'Nota' => 10,
+    ],
+    [
+        'Aluno' => 'Augusto',
+        'Nota' => 6,
+    ],
+    [
+        'Aluno' => 'Ricardo',
+        'Nota' => 9,
+    ],
 ];
-// fiz uma copia das notas:
-$notasOrdenadas = $notas;
-// coloquei eles em ordem:
-sort($notasOrdenadas);
 
-echo "Oredenadas:";
-var_dump($notasOrdenadas);
+function ordenaNotas(array $nota1, array $nota2): int
+{
+    if ($nota1['Nota'] > $nota2['Nota']) {
+        return -1;
+    }
 
-echo "Desordenadas:";
+    if ($nota2['Nota'] > $nota1['Nota']) {
+        return 1;
+    }
+
+    return 0;
+}
+
+usort($notas, 'ordenaNotas');
 var_dump($notas);
